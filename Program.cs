80fd96868dee0daab66e673db38d23f6458da7b9
Jorisanlo27 <jorgersantana27@gmail.com>
2023-03-20 11:24:27 -0400

    CodeGeneration Package Installed and DbContext changed in program class

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Universidad.Data;
+using Universidad.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<UniversidadContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<UniversidadContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
