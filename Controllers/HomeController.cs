a3c3008d0d7fc07e108cca28480a9b7a981471d0
CNelson29 <c.tiburciomolano@gmail.com>
2023-03-23 00:59:58 -0400

    HomeController - Dashboard Method - Added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,10 @@ namespace Universidad.Controllers
         {
             return View();
         }
-
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
