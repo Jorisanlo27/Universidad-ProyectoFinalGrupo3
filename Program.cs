8043e1e886f60b2b12f0a6858affe603b9c83349
Jorisanlo27 <jorgersantana27@gmail.com>
2023-04-03 11:49:20 -0400

    Program - Change Default Page From 'Index' To 'Dashboard' - Updated

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Dashboard}/{id?}");
 app.MapRazorPages();
 
 app.Run();
