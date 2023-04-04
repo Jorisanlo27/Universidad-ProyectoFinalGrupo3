bc7ff27e54a9fe084555dc0ecc96cc047e971518
Jorisanlo27 <jorgersantana27@gmail.com>
2023-04-03 20:10:53 -0400

    Program - Route Pattern Set Action to Index - Updated

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Dashboard}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
