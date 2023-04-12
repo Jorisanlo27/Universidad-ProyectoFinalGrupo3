619e5b9c0fbed8f157b4321fdb308e1da9b19a7f
Jorisanlo27 <jorgersantana27@gmail.com>
2023-04-12 17:41:23 -0400

    Subjects MVC - Added

---
+++ Controllers/SubjectsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Universidad.Models;
+
+namespace Universidad.Controllers
+{
+    public class SubjectsController : Controller
+    {
+        private readonly UniversidadContext _context;
+
+        public SubjectsController(UniversidadContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Subjects
+        public async Task<IActionResult> Index()
+        {
+            var universidadContext = _context.Asignaturas.Include(a => a.IdAreaNavigation);
+            return View(await universidadContext.ToListAsync());
+        }
+
+        // GET: Subjects/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Asignaturas == null)
+            {
+                return NotFound();
+            }
+
+            var asignatura = await _context.Asignaturas
+                .Include(a => a.IdAreaNavigation)
+                .FirstOrDefaultAsync(m => m.IdAsignatura == id);
+            if (asignatura == null)
+            {
+                return NotFound();
+            }
+
+            return View(asignatura);
+        }
+
+        // GET: Subjects/Create
+        public IActionResult Create()
+        {
+            ViewData["IdArea"] = new SelectList(_context.Areas, "IdArea", "IdArea");
+            return View();
+        }
+
+        // POST: Subjects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdAsignatura,IdArea,Codigo,Nombre,Creditos,PreRequisitos,Metodo")] Asignatura asignatura)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(asignatura);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdArea"] = new SelectList(_context.Areas, "IdArea", "IdArea", asignatura.IdArea);
+            return View(asignatura);
+        }
+
+        // GET: Subjects/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Asignaturas == null)
+            {
+                return NotFound();
+            }
+
+            var asignatura = await _context.Asignaturas.FindAsync(id);
+            if (asignatura == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdArea"] = new SelectList(_context.Areas, "IdArea", "IdArea", asignatura.IdArea);
+            return View(asignatura);
+        }
+
+        // POST: Subjects/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdAsignatura,IdArea,Codigo,Nombre,Creditos,PreRequisitos,Metodo")] Asignatura asignatura)
+        {
+            if (id != asignatura.IdAsignatura)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(asignatura);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AsignaturaExists(asignatura.IdAsignatura))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdArea"] = new SelectList(_context.Areas, "IdArea", "IdArea", asignatura.IdArea);
+            return View(asignatura);
+        }
+
+        // GET: Subjects/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Asignaturas == null)
+            {
+                return NotFound();
+            }
+
+            var asignatura = await _context.Asignaturas
+                .Include(a => a.IdAreaNavigation)
+                .FirstOrDefaultAsync(m => m.IdAsignatura == id);
+            if (asignatura == null)
+            {
+                return NotFound();
+            }
+
+            return View(asignatura);
+        }
+
+        // POST: Subjects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Asignaturas == null)
+            {
+                return Problem("Entity set 'UniversidadContext.Asignaturas'  is null.");
+            }
+            var asignatura = await _context.Asignaturas.FindAsync(id);
+            if (asignatura != null)
+            {
+                _context.Asignaturas.Remove(asignatura);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AsignaturaExists(int id)
+        {
+          return (_context.Asignaturas?.Any(e => e.IdAsignatura == id)).GetValueOrDefault();
+        }
+    }
+}
